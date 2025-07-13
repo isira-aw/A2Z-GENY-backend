deff1eccb0c911f9ca663048eb2f21863e1a412a
Isira Adithya Weerasinghe <isira.aw@gmail.com>
2025-07-13 23:53:13 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/RegistrationLoginPage/config/SecurityConfig.java
+++ src/main/java/com/example/RegistrationLoginPage/config/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://geny.netlify.app","http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
