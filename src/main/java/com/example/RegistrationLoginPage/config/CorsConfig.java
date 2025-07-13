55004017910d089be78a2453dceda002bbf66cd6
Isira Adithya Weerasinghe <isira.aw@gmail.com>
2025-07-13 23:09:20 +0530

    set database

--- src/main/java/com/example/RegistrationLoginPage/config/CorsConfig.java
+++ src/main/java/com/example/RegistrationLoginPage/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:5173") // Allow React frontend
+                .allowedOrigins("https://geny.netlify.app","http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
